628be750e1de145cb3ee2e3ea4ee2fe0095953e8
admin <>
2023-04-17 17:09:26 +0800

    Added record delete interface

--- src/pages/Traffic/Traffic.tsx
+++ src/pages/Traffic/Traffic.tsx
@@ -6,6 +6,7 @@ import repeaterApi, {TrafficItem} from "@/services/Repeater";
 import {useHistory} from "react-router-dom";
 import {TablePaginationConfig} from "antd/lib/table/interface";
 import ReplayModal from "@/pages/Traffic/ReplayModal";
+import TrafficService from "@/pages/Traffic/TrafficService";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const Traffic = () => {
@@ -14,29 +15,38 @@ const Traffic = () => {
     const [currentPage, setCurrentPage] = useState<number>(1);
     const [modalVisible, setModalVisible] = useState(false);
     const [modalProps, setModalProps] = useState({appName: "", traceId: ""});
+    const getTrafficList = async () => {
+        try {
+            let repeaterResp = await repeaterApi.getOnline(currentPage);
+            if (!repeaterResp.success) {
+                message.error(repeaterResp.message || "网络错误，请稍后再试。");
+                return;
+            }
+            setDataSource(repeaterResp.data);
+            setTotal(repeaterResp.count);
+        } catch (e) {
+            message.error("发生了错误");
+        }
+    }
 
     useEffect(() => {
-        const getTrafficList = async () => {
-            try {
-                let repeaterResp = await repeaterApi.getOnline(currentPage);
-                if (!repeaterResp.success) {
-                    message.error(repeaterResp.message || "网络错误，请稍后再试。");
-                    return;
-                }
-                setDataSource(repeaterResp.data);
-                setTotal(repeaterResp.count);
-            } catch (e) {
-                message.error("发生了错误");
-            }
-        };
-
         void getTrafficList();
-    }, [currentPage]);
+        },[currentPage]);
 
     let history = useHistory();
 
     const detailClicked = (traceId: string, appName: string) => {
         history.push("/trafficDetails?id=" + traceId + "&appName=" + appName);
+    };
+    const deleteClicked = async (id: number, appName: string) => {
+        const resp = await TrafficService.delRecord(id);
+        if(resp.success) {
+            message.info("删除成功：" + id);
+            void await getTrafficList();
+            return;
+        } else {
+            message.warn(resp.message);
+        }
     };
 
     const columns: ColumnsType<TrafficItem> = [{
@@ -67,16 +77,19 @@ const Traffic = () => {
     }, {
         title: "操作",
         key: "action",
-        width: 140,
+        width: 200,
         render: (_, record) => {
             return (<div className={styles.ActionPanel}>
                 <Button type={"primary"} size="small" onClick={() => {
                     detailClicked(record.traceId, record.appName);
                 }}>详情</Button>
-                <Button type={"primary"} size="small" danger onClick={() => {
+                <Button type={"primary"} size="small" onClick={() => {
                     setModalProps({appName: record.appName, traceId: record.traceId});
                     setModalVisible(true);
                 }}>回放</Button>
+                <Button type={"primary"} size="small" danger onClick={() => {
+                    deleteClicked(record.id, record.appName);
+                }}>删除</Button>
             </div>);
         },
     }];
